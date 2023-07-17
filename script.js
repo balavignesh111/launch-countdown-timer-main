ab68a5e2f789dfeddc4e60b4bec06cdee689283c
balavignesh111 <balavignesh1018@gmail.com>
2023-07-17 23:46:07 +0530

    css almost completed

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-'use strict';
+// 'use strict';
+
+// // const endDate = Date.parse("25 Aug 2023 05:12:00");
+// const endDate = (Date.now() + 1209600000);
+// console.log(endDate);
+
+// let currentTime = Date.now(); // it returns milliseconds
+//   console.log(currentTime);
+
+// let difference = endDate - currentTime;
+// console.log(difference);
+// let days = 0;
+// let hours = 0;
+// let minutes = 0;
+// let seconds = 0;
+// // const currentTime = Date.now(); // it returns milliseconds
+// // console.log(currentTime);
+
+// // let difference = endDate - currentTime;
+// // console.log(difference);
+// // let days = 0;
+// // let hours = 0;
+// // let minutes = 0;
+// // let seconds = 0;
+// // days = (difference / (24 * 60 * 60 * 1000));
+// // difference = (difference % (24 * 60 * 60 * 1000));
+// // console.log(Math.floor(days));
+// // hours = (difference / (60 * 60 * 1000));
+// // difference = (difference % (60 * 60 * 1000));
+// // minutes = (difference / (60 * 1000));
+// // difference = (difference % (60 * 1000));
+// // seconds = (difference / (1000));
+// // difference = (difference % (1000));
+
+// // console.log(`no of days is ${days} and hrs : ${hours} min ${minutes} sec ${seconds}`)
+// setInterval(()=> {
+// days = (difference / (24 * 60 * 60 * 1000));
+// difference = (difference % (24 * 60 * 60 * 1000));
+// console.log(Math.floor(days));
+// hours = (difference / (60 * 60 * 1000));
+// difference = (difference % (60 * 60 * 1000));
+// minutes = (difference / (60 * 1000));
+// difference = (difference % (60 * 1000));
+// seconds = (difference / (1000));
+// difference = (difference % (1000));
+
+//   console.log(`no of days is ${Math.floor(days)} and hrs : ${Math.floor(hours)} min ${Math.floor(minutes)} sec ${Math.floor(seconds)}`)
+// },1000)
